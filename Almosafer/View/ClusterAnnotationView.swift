ff133d499b9f6dfac93e05a35f0f5be1b80ca7c3
Fawzi <fawzialrifai@icloud.com>
2022-03-17 18:01:04 +0300

    Improve the size and animation of the clustering annotation view.

--- Almosafer/View/ClusterAnnotationView.swift
+++ Almosafer/View/ClusterAnnotationView.swift
@@ -10,18 +10,30 @@ import MapKit
 
 class ClusterAnnotationView: MKAnnotationView {
     
+    override init(annotation: MKAnnotation?, reuseIdentifier: String?) {
+        super.init(annotation: annotation, reuseIdentifier: reuseIdentifier)
+        displayPriority = .defaultHigh
+        collisionMode = .circle
+        centerOffset = CGPoint(x: 0, y: -10) // Offset center point to animate better with marker annotations
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     override var annotation: MKAnnotation? {
         willSet {
-            canShowCallout = false
             if let cluster = newValue as? MKClusterAnnotation {
-                let renderer = UIGraphicsImageRenderer(size: CGSize(width: 40, height: 40))
+                let annotationsCount = "\(cluster.memberAnnotations.count)"
+                let size = CGSize(width: annotationsCount.size().width + 24, height: annotationsCount.size().width + 24)
+                canShowCallout = false
+                let renderer = UIGraphicsImageRenderer(size: size)
                 image = renderer.image { _ in
                     // Fill circle with color
                     UIColor(named: "AlmosaferColor")?.setFill()
-                    UIBezierPath(ovalIn: CGRect(x: 0, y: 0, width: 40, height: 40)).fill()
+                    UIBezierPath(ovalIn: CGRect(x: 0, y: 0, width: size.width, height: size.height)).fill()
                     // Draw count text vertically and horizontally centered
-                    let annotationsCount = "\(cluster.memberAnnotations.count)"
-                    let rect = CGRect(x: 20 - annotationsCount.size().width / 2, y: 20 - annotationsCount.size().height / 2, width: annotationsCount.size().width, height: annotationsCount.size().height)
+                    let rect = CGRect(x: size.width / 2 - annotationsCount.size().width / 2, y: size.height / 2 - annotationsCount.size().height / 2, width: size.width, height: size.height)
                     annotationsCount.draw(in: rect, withAttributes: [NSAttributedString.Key.foregroundColor: UIColor.white])
                 }
             }
